565bfea0a02d30ab5476d92ff9aa3749f2185efa
Luke Burton <luke.burton@apple.com>
2024-01-02 09:42:01 -0800

    foo

--- flake.nix
+++ flake.nix
@@ -6,32 +6,13 @@
   };
 
   outputs = { self, pkgs, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
 
-        naersk' = pkgs.callPackage naersk {};
-
-        isLinux = pkgs.lib.system.isLinux system;
-
-      in rec {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-        nativeBuildInputs = with pkgs; [ pkg-config ];
-        buildInputs = with pkgs; [ openssl dbus ];
-          src = ./.;
-        };
-
-        # For `nix develop`:
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
-        };
-
-        nixosModule = if isLinux then { config, lib, pkgs, ... }:
+    flake-utils.lib.eachSystem [ system.x86_64-linux ] (system: {
+        nixosModule = { config, lib, pkgs, ... }:
         with lib;
         let cfg = config.services.blueplug;
+        pkgs = nixpkgs.legacyPackages.${system};
+
         in {
             options.services.blueplug = {
                 enable = mkEnableOption "BTLE Plug";
@@ -60,7 +41,7 @@
                     description = "BTLE Plug";
                     wantedBy = ["multi-user.target"];
                     serviceConfig = {
-                        ExecStart = "${defaultPackage}/bin/btleplug --client_id ${cfg.client_id} --mqtt-addr ${cfg.mqtt_address} --mqtt-port ${toString cfg.mqtt_port}";
+                        ExecStart = "${pkgs.blueplug}/bin/btleplug --client_id ${cfg.client_id} --mqtt-addr ${cfg.mqtt_address} --mqtt-port ${toString cfg.mqtt_port}";
                         ProtectHome = "read-only";
                         Restart = "on-failure";
                         Type = "exec";
@@ -68,7 +49,29 @@
                 };
             };
 
-        }
-        else null;
+        };
+    })
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+
+      in rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+        nativeBuildInputs = with pkgs; [ pkg-config ];
+        buildInputs = with pkgs; [ openssl dbus ];
+          src = ./.;
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustc cargo ];
+        };
+
       });
 }
