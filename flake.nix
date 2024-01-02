068d5a5372ac919f774ea91311d8758bb7329de0
Luke Burton <luke.burton@apple.com>
2024-01-02 09:57:03 -0800

    foo

--- flake.nix
+++ flake.nix
@@ -7,51 +7,6 @@
 
   outputs = { self, pkgs, flake-utils, naersk, nixpkgs }:
 
-    flake-utils.lib.eachSystem [ system.x86_64-linux ] (system: {
-        nixosModule = { config, lib, pkgs, ... }:
-        with lib;
-        let cfg = config.services.blueplug;
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        in rec {
-            options.services.blueplug = {
-                enable = mkEnableOption "BTLE Plug";
-
-                client_id = mkOption {
-                    type = types.str;
-                    default = "";
-                    description = "MQTT Client ID";
-                };
-
-                mqtt_address = mkOption {
-                    type = types.str;
-                    default = "";
-                    description = "MQTT Address";
-                };
-
-                mqtt_port = mkOption {
-                    type = types.port;
-                    default = 1883;
-                    description = "MQTT Port";
-                };
-            };
-
-            config = mkIf cfg.enable {
-                systemd.services.btleplug = {
-                    description = "BTLE Plug";
-                    wantedBy = ["multi-user.target"];
-                    serviceConfig = {
-                        ExecStart = "${pkgs.blueplug}/bin/btleplug --client_id ${cfg.client_id} --mqtt-addr ${cfg.mqtt_address} --mqtt-port ${toString cfg.mqtt_port}";
-                        ProtectHome = "read-only";
-                        Restart = "on-failure";
-                        Type = "exec";
-                    };
-                };
-            };
-
-        };
-    })
-
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs) {
@@ -73,5 +28,48 @@
           nativeBuildInputs = with pkgs; [ rustc cargo ];
         };
 
-      });
+      })
+      // {
+         nixosModule = { config, lib, pkgs, ... }:
+         with lib;
+         let cfg = config.services.blueplug;
+
+         in rec {
+             options.services.blueplug = {
+                 enable = mkEnableOption "BTLE Plug";
+
+                 client_id = mkOption {
+                     type = types.str;
+                     default = "";
+                     description = "MQTT Client ID";
+                 };
+
+                 mqtt_address = mkOption {
+                     type = types.str;
+                     default = "";
+                     description = "MQTT Address";
+                 };
+
+                 mqtt_port = mkOption {
+                     type = types.port;
+                     default = 1883;
+                     description = "MQTT Port";
+                 };
+             };
+
+             config = mkIf cfg.enable {
+                 systemd.services.btleplug = {
+                     description = "BTLE Plug";
+                     wantedBy = ["multi-user.target"];
+                     serviceConfig = {
+                         ExecStart = "${pkgs.blueplug}/bin/btleplug --client_id ${cfg.client_id} --mqtt-addr ${cfg.mqtt_address} --mqtt-port ${toString cfg.mqtt_port}";
+                         ProtectHome = "read-only";
+                         Restart = "on-failure";
+                         Type = "exec";
+                     };
+                 };
+             };
+
+         };
+     };
 }
